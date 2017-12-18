71cd042bc27cb71d44d4ffacf3b7b3aa5effee1c
Anduin Mooney <anduinmooney@gmail.com>
2017-12-17 23:04:34 -0800

    Alert user whenever a '1' is included in the index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 //backend
 var numbers = [];
-var beepBoop = function (input) { 
+var beepBoop = function (input) {
   for (var i = 0; i <= input; i++) { //Check if the current index is less than the given input. If it is, then continue increasing index
     numbers.push(i); //pushes all numbers in index to array numbers
     if (i.toString().includes("0")) { //Check if the number listed in the index includes a 0
       alert("Beep!");
-    }
+    } else if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
+        alert("Boop!");
+    } 
 
   console.log(input);
   console.log(numbers);
