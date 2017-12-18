f55bac28ec1219f15b9b6eb89628746ddb19c9a2
Anduin Mooney <anduinmooney@gmail.com>
2017-12-17 23:22:41 -0800

    Add results to page instead of alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,19 @@ var numbers = [];
 var beepBoop = function (input) {
   for (var i = 0; i <= input; i++) { //Check if the current index is less than the given input. If it is, then continue increasing index
     numbers.push(i); //pushes all numbers in index to array numbers
-    if (i.toString().includes("0")) { //Check if the number listed in the index includes a 0
-      alert("Beep!");
-    } else if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
-        alert("Boop!");
+    if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
+        numbers[i] = "Boop, ";
+    }
+    else if (i.toString().includes("0")) { //Check if the number listed in the index includes a 0
+          numbers[i] = "Beep, ";
     } else if (i % 3 === 0) { //If the current index number divided by 3 will set the index number to 0, then alert user with "I'm sorry, Dave. I'm afraid I can't do that."
-        alert("I'm sorry, Dave. I'm afraid I can't do that.");
+        numbers[i] = "I'm sorry, Dave. I'm afraid I can't do that. , ";
     }
 
   console.log(input);
   console.log(numbers);
 }
-return numbers;
+return numbers; //returns number array
 }
 
 
