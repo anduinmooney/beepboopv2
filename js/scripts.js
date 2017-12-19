c0d6a839537b8be3ea84f88e86938c15893834ac
Guest User <Guest@Epicodus-9E.local>
2017-12-19 08:20:36 -0800

    Fix issue where the 0 would display the wrong message.

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,16 @@ var beepBoop = function (input) {
   for (var i = 0; i <= input; i++) { //Check if the current index is less than the given input. If it is, then continue increasing index
     numbers.push(i); //pushes all numbers in index to array numbers
 
-    if (i % 3 === 0) { //If the current index number divided by 3 will set the index number to 0, then alert user with "I'm sorry, Dave. I'm afraid I can't do that."
-            numbers[i] ="<li>" + "I'm sorry, Dave. I'm afraid I can't do that. , " + "</li>";
+    if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
+      numbers[i] = "<li>" + "Boop, " + "</li>";
 
     }
     else if (i.toString().includes("0")) { //Check if the number listed in the index includes a 0
-          numbers[i] = "<li>" + "Beep, " + "</li>";
+      numbers[i] = "<li>" + "Beep, " + "</li>";
 
     }
-    else if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
-        numbers[i] = "<li>" + "Boop, " + "</li>";
+    else if (i % 3 === 0) { //If the current index number divided by 3 will set the index number to 0, then alert user with "I'm sorry, Dave. I'm afraid I can't do that."
+      numbers[i] ="<li>" + "I'm sorry, Dave. I'm afraid I can't do that. , " + "</li>";
 
     }
     else {
