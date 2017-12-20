8393be1adc3caf7ee7229e56608557ad8473219b
Anduin Mooney <anduinmooney@gmail.com>
2017-12-19 19:52:30 -0800

    Seperate user end from backend, as well as change userend to insert list with every number added instead of doing that in the backend.

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,18 @@ var beepBoop = function (input) {
   for (var i = 0; i <= input; i++) { //Check if the current index is less than the given input. If it is, then continue increasing index
     numbers.push(i); //pushes all numbers in index to array numbers
 
-    if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
-      numbers[i] = "<li>" + "Boop, " + "</li>";
 
-    }
-    else if (i.toString().includes("0")) { //Check if the number listed in the index includes a 0
-      numbers[i] = "<li>" + "Beep, " + "</li>";
+    if (i.toString().includes("0")) { //Check if the number listed in the index includes a 0
+      numbers[i] = beep;
 
-    }
-    else if (i % 3 === 0) { //If the current index number divided by 3 will set the index number to 0, then alert user with "I'm sorry, Dave. I'm afraid I can't do that."
-      numbers[i] ="<li>" + "I'm sorry, Dave. I'm afraid I can't do that. , " + "</li>";
+    } else if (i.toString().includes("1")) { //Check if the number listed in the index includes a 1
+      numbers[i] = boop;
 
-    }
-    else {
-      numbers[i] = "<li>" + i + "</li>"; //puts regular digits into
+    } else if (i % 3 === 0) { //If the current index number divided by 3 will set the index number to 0, then alert user with "I'm sorry, Dave. I'm afraid I can't do that."
+      numbers[i] = dave;
+
+    } else {
+      numbers[i] = i ; //puts regular digits into list.
     }
 
   console.log(input);
@@ -29,17 +27,21 @@ return numbers; //returns number array
 
 
 //userend
-
+var boop = "Boop, ";
+var beep = "Beep, ";
+var dave = "I'm sorry, Dave. I'm afraid I can't do that. , ";
 $(document).ready(function() {
-    $("form#form1").submit(function(event) {
-      event.preventDefault();
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    var resultArray = [];
+    var input = parseInt($("input#input1").val());
+    var result = beepBoop(input);
+    $("#results").empty();
+    numbers.forEach(function(resultArray) {
+      $("#results").append("<li>" + resultArray + "</li>");
+      });
 
-      var input = parseInt($("input#input1").val());
-      var result = beepBoop(input);
-      $("#results").append(result);
-      $("#results").last().click(function() { //clickable
-      $("#results").empty();
+
 
     });
     });
-});
